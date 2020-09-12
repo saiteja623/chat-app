090653f3a72e3afffe78a5c2d1a0b2b5686eca44
saiteja623 <saiteja.balla13@gmail.com>
2020-09-12 14:08:38 +0530

    Updates

---
+++ precache-manifest.cc7b818493e246dc9e7157341b4e4a49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2828bf2b67d88d1987406623b4397e56",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "409144f00289cf1dea27",
+    "url": "/chat-app/static/css/main.1aa52578.chunk.css"
+  },
+  {
+    "revision": "71021077fe4284b94b05",
+    "url": "/chat-app/static/js/2.2fa18f69.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/chat-app/static/js/2.2fa18f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "409144f00289cf1dea27",
+    "url": "/chat-app/static/js/main.91b122f4.chunk.js"
+  },
+  {
+    "revision": "0600bf7f29860d9b2dd4",
+    "url": "/chat-app/static/js/runtime-main.c5bf7b60.js"
+  }
+]);
